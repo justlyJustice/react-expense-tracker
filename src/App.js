e59d4ba3e5f97b1c00e07c7499ba0265a04a9dd0
boungbai-computers <info@boungbai.com>
2021-10-12 11:32:45 +0100

    First file commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+
+import { GlobalProvider } from "./context/GlobalState";
+
+import "./App.css";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
